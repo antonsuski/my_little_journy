c663e57b8537234da6a638f92df33aa6d762dec5
Anton Suski <antonsyski@gmail.com>
2021-01-30 13:58:17 +0300

    step_9

--- step_1/math_functions/mysqrt.cxx
+++ step_1/math_functions/mysqrt.cxx
@@ -1,1 +1,19 @@
+#include "qtsqrt.hxx"
 
+#include <cmath>
+
+#ifdef USE_MYMATH
+#include "mysqrt.hxx"
+#endif
+
+namespace matlib
+{
+double sqrt(double x)
+{
+#ifdef USE_MYMATH
+    return detail::mysqrt(x);
+#else
+    return std::sqrt(x);
+#endif
+}
+} // namespace matlib
