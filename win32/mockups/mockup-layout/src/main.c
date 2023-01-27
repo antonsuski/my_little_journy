39337ccf20e0905042ff97a8955afd7cfb1ca12b
Anton Suskiy <anton.suskiy@psa.inc>
2023-01-27 13:19:34 +0300

    Adds refacoring

--- win32/mockups/mockup-layout/src/main.c
+++ win32/mockups/mockup-layout/src/main.c
@@ -6,33 +6,32 @@
 
 #include <windows.h>
 
-#include <stdio.h>
-
 LRESULT CALLBACK wnd_proc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam);
 
-int WINAPI wWinMain(HINSTANCE hinstance, HINSTANCE h_prev_instance,
-                    PWSTR cmd_line, int cmd_show)
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                    PWSTR pCmdLine, int nCmdShow)
 {
     // Create and register window class
     WNDCLASS      wc           = {};
-    const wchar_t class_name[] = L"Sample Window Class";
+    const wchar_t class_name[] = L"window";
 
     wc.lpfnWndProc   = wnd_proc;
-    wc.hInstance     = hinstance;
+    wc.hInstance     = hInstance;
     wc.lpszClassName = class_name;
 
     RegisterClass(&wc);
 
     // Create window
-    HWND main_window = CreateWindowEx(
-        0, class_name, L"Title", WS_OVERLAPPED, CW_USEDEFAULT, CW_USEDEFAULT,
-        CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hinstance, NULL);
+    HWND main_window =
+        CreateWindowEx(0, class_name, L"Title", WS_OVERLAPPEDWINDOW,
+                       CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+                       CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
     if (main_window == NULL)
     {
         return 0;
     }
 
-    ShowWindow(main_window, cmd_show);
+    ShowWindow(main_window, nCmdShow);
 
     MSG msg = {};
 
