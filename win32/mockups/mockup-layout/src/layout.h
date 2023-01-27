10ef190b44f61021622436344aff08b5e7fa1c4c
Anton Suskiy <anton.suskiy@psa.inc>
2023-01-27 17:12:39 +0300

    adds buttons for layout

--- win32/mockups/mockup-layout/src/layout.h
+++ win32/mockups/mockup-layout/src/layout.h
@@ -1,18 +1,23 @@
-#include "Windows.h"
-#include "math.h"
+#include "windows.h"
 
-typedef enum {
+typedef enum
+{
     HORIZONTAL = 1,
-    VERTICAL = 2,
-    GRID = 3,
+    VERTICAL   = 2,
+    GRID       = 3,
 } layout_t;
 
 typedef struct
 {
-    HWND* hwnd;
-    POINT window_size;
-    POINT window_position;
-    POINT layout_size;
-    POINT layout_position;
+    HWND*    hwnd;
+    POINT    window_size;
+    POINT    window_position;
+    POINT    layout_size;
+    POINT    layout_position;
     layout_t layout_type;
-}window_descriptro_t;
+} window_descriptro_t;
+
+typedef struct
+{
+    window_descriptro_t wind_desc;
+} layout_control_t;
