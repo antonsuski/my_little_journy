d322960562ee24f5a0fd81a2fc1433ce3bb24060
Anton Suskiy <anton.suskiy@psa.inc>
2023-01-27 11:47:20 +0300

    Added window sample

--- win32/mockups/mockup-layout/src/main.c
+++ win32/mockups/mockup-layout/src/main.c
@@ -1,24 +1,32 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
 
-#include <Windows.h>
+#include "layout.h"
+
+#include <windows.h>
 
 #include <stdio.h>
 
 LRESULT CALLBACK wnd_proc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam);
 
-int WINAPI wWinMain(HINSTANCE hinstance, HINSTANCE h_prev_instance, PWSTR cmd_line, int cmd_show)
+int WINAPI wWinMain(HINSTANCE hinstance, HINSTANCE h_prev_instance,
+                    PWSTR cmd_line, int cmd_show)
 {
     // Create and register window class
-    WNDCLASS wc  = {};
-    const wchar_t class_name[] = L"window";
+    WNDCLASS      wc           = {};
+    const wchar_t class_name[] = L"Sample Window Class";
 
-    wc.lpfnWndProc = wnd_proc;
-    wc.hInstance = hinstance;
+    wc.lpfnWndProc   = wnd_proc;
+    wc.hInstance     = hinstance;
     wc.lpszClassName = class_name;
 
     RegisterClass(&wc);
 
     // Create window
-    HWND main_window = CreateWindowEx(0, class_name,L"Title", WS_OVERLAPPED, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,CW_USEDEFAULT,NULL,NULL,hinstance,NULL);
+    HWND main_window = CreateWindowEx(
+        0, class_name, L"Title", WS_OVERLAPPED, CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hinstance, NULL);
     if (main_window == NULL)
     {
         return 0;
@@ -33,11 +41,27 @@ int WINAPI wWinMain(HINSTANCE hinstance, HINSTANCE h_prev_instance, PWSTR cmd_li
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-    
+
     return 0;
 }
 
 LRESULT CALLBACK wnd_proc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam)
 {
-    return 0;
+    switch (umsg)
+    {
+        case WM_DESTROY:
+        {
+            PostQuitMessage(0);
+        }
+            return 0;
+        case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC         hdc = BeginPaint(hwnd, &ps);
+            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+            EndPaint(hwnd, &ps);
+        }
+            return 0;
+    }
+    return DefWindowProc(hwnd, umsg, wparam, lparam);
 }
