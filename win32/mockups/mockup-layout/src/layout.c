3fc163ad8866a7c11864e0a6773f1b9cb685f169
Anton Suskiy <anton.suskiy@psa.inc>
2023-02-02 15:43:40 +0300

    kk

--- win32/mockups/mockup-layout/src/layout.c
+++ win32/mockups/mockup-layout/src/layout.c
@@ -7,16 +7,23 @@
 
 void create_layout_mesh(layout_mesh_t* mesh)
 {
-    // if (mesh->mesh_format.x == 0)
-    // {
-    //     mesh->mesh_format.x = 1;
-    //     // inseart warning log;
-    // }
-    // if (mesh->mesh_format.y == 0)
-    // {
-    //     mesh->mesh_format.y = 1;
-    //     // inseart warning log;
-    // }
+    if (mesh->layout_type == HORIZONTAL && (mesh->mesh_format.y > mesh->mesh_format.x))
+    {
+        mesh->mesh_format.y = mesh->mesh_format.x ^ mesh->mesh_format.y;
+        mesh->mesh_format.x = mesh->mesh_format.x ^ mesh->mesh_format.y;
+        mesh->mesh_format.y = mesh->mesh_format.y ^ mesh->mesh_format.x;
+    }
+    
+    if (mesh->mesh_format.x == 0 || mesh->layout_type == VERTICAL)
+    {
+        mesh->mesh_format.x = 1;
+        // inseart warning log;
+    }
+    if (mesh->mesh_format.y == 0 || mesh->layout_type == HORIZONTAL)
+    {
+        mesh->mesh_format.y = 1;
+        // inseart warning log;
+    }
     mesh->elements = (layout_element_t**)malloc(mesh->mesh_format.x *
                                                 sizeof(layout_element_t*));
     for (size_t i = 0; i < mesh->mesh_format.x; i++)
@@ -45,13 +52,14 @@ void delete_layout_mesh(layout_mesh_t* mesh)
 
 void calculate_layout(layout_mesh_t* mesh)
 {
+    POINT element_size;
+    element_size.x = mesh->mesh_size.x / mesh->mesh_format.x;
+    element_size.y = mesh->mesh_size.y / mesh->mesh_format.y;
+
     switch (mesh->layout_type)
     {
         case HORIZONTAL:
         {
-            POINT element_size;
-            element_size.x = mesh->mesh_size.x / mesh->mesh_format.x;
-            element_size.y = mesh->mesh_size.y;
             for (size_t i = 0; i < mesh->mesh_format.x; i++)
             {
                 mesh->elements[i][0].layout_size.x = element_size.x;
@@ -63,11 +71,22 @@ void calculate_layout(layout_mesh_t* mesh)
                 mesh->elements[i][0].layout_position_in_mesh.x = i;
                 mesh->elements[i][0].layout_position_in_mesh.y = 0;
             }
-
-            return;
         }
         break;
-
+        case VERTICAL:
+        {
+            for (size_t i = 0; i < mesh->mesh_format.y; i++)
+            {
+                mesh->elements[0][i].layout_size.x = element_size.x;
+                mesh->elements[0][i].layout_size.y = element_size.y;
+                mesh->elements[0][i].layout_position.x =
+                    0 + mesh->mesh_position.x;
+                mesh->elements[0][i].layout_position.y =
+                    i * element_size.x + mesh->mesh_position.y;
+                mesh->elements[0][i].layout_position_in_mesh.x = 0;
+                mesh->elements[0][i].layout_position_in_mesh.y = i;
+            }
+        }
         default:
             return;
     }
--- win32/mockups/mockup-layout/src/mockup-layout.c
+++ win32/mockups/mockup-layout/src/mockup-layout.c
@@ -6,10 +6,16 @@
 
 #include <windows.h>
 
+//statics
 static layout_mesh_t mesh;
 static WNDPROC origin_btn_proc = NULL;
+//buffer
+WCHAR buffer[1024] = {0};
+//proc
 LRESULT CALLBACK wnd_proc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam);
 LRESULT CALLBACK btn_proc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam);
+
+BOOL CALLBACK EnumChildProc(HWND hwnd, LPARAM lparam);
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
                     PWSTR pCmdLine, int nCmdShow)
@@ -27,28 +33,23 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     // Create window
     HWND main_window = CreateWindowEx(
         0, class_name, L"Title", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
-        CW_USEDEFAULT, 315, 338, NULL, NULL, hInstance, NULL);
+        CW_USEDEFAULT, 315, 500, NULL, NULL, hInstance, NULL);
     if (main_window == NULL)
     {
         return 0;
     }
     // Create mesh
-    mesh.mesh_format.x   = 2;
+    mesh.mesh_format.x   = 3;
     mesh.mesh_format.y   = 1;
     mesh.layout_type     = HORIZONTAL;
     mesh.mesh_position.x = 50;
     mesh.mesh_position.y = 50;
-    mesh.mesh_size.x     = 300;
+    mesh.mesh_size.x     = 200;
     mesh.mesh_size.y     = 100;
     create_layout_mesh(&mesh);
     calculate_layout(&mesh);
 
-    WNDCLASS wbc = {};
     const wchar_t wbc_name[] = L"BUTTON";
-    // wbc.lpfnWndProc = btn_proc;
-    // wbc.hInstance = hInstance;
-    // wbc.lpszClassName = wbc_name;
-    // RegisterClass(&wbc);
     HWND button1 = CreateWindow(wbc_name, L"text",
                                   WS_CHILD | WS_VISIBLE | BS_DEFPUSHBUTTON, 0, 0, 100, 100,
                                   main_window, NULL, hInstance, NULL);
@@ -60,12 +61,16 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     add_control_into_mesh(&mesh, button1);
     origin_btn_proc = (WNDPROC)SetWindowLongPtr(button1, GWLP_WNDPROC, (LONG_PTR)btn_proc);
 
-    // HWND button2 = CreateWindowEx(WS_EX_TRANSPARENT, L"button", L"text",
-    //                               WS_CHILD | WS_VISIBLE, 0, 100, 100, 100,
-    //                               main_window, NULL, hInstance, NULL);
-    // HWND button3 = CreateWindowEx(WS_EX_TRANSPARENT, L"button", L"text",
-    //                               WS_CHILD | WS_VISIBLE, 0, 200, 100, 100,
-    //                               main_window, NULL, hInstance, NULL);
+    HWND button2 = CreateWindowEx(WS_EX_TRANSPARENT, L"button", L"text",
+                                  WS_CHILD | WS_VISIBLE, 0, 100, 100, 100,
+                                  main_window, NULL, hInstance, NULL);
+    add_control_into_mesh(&mesh, button2);
+    origin_btn_proc = (WNDPROC)SetWindowLongPtr(button2, GWLP_WNDPROC, (LONG_PTR)btn_proc);
+    HWND button3 = CreateWindowEx(WS_EX_TRANSPARENT, L"button", L"text",
+                                  WS_CHILD | WS_VISIBLE, 0, 200, 100, 100,
+                                  main_window, NULL, hInstance, NULL);
+    add_control_into_mesh(&mesh, button3);
+    origin_btn_proc = (WNDPROC)SetWindowLongPtr(button3, GWLP_WNDPROC, (LONG_PTR)btn_proc);
     // HWND button4 = CreateWindowEx(WS_EX_TRANSPARENT, L"button", L"text",
     //                               WS_CHILD | WS_VISIBLE, 100, 0, 100, 100,
     //                               main_window, NULL, hInstance, NULL);
@@ -116,46 +121,85 @@ LRESULT CALLBACK wnd_proc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam)
             EndPaint(hwnd, &ps);
             return 0;
         }
+        case WM_SIZING:
+        {
+            RECT rect;
+            GetWindowRect(hwnd, &rect);
+            mesh.mesh_size.x = abs(rect.right - rect.left) - (2 * mesh.mesh_position.x);
+            calculate_layout(&mesh);
+            EnumChildWindows(hwnd, EnumChildProc, lparam);
+            // RedrawWindow(hwnd, NULL, NULL, RDW_UPDATENOW | RDW_ALLCHILDREN);
+            return 0;
+        }
     }
     return DefWindowProc(hwnd, umsg, wparam, lparam);
 }
 
+// #define TRACK_MOUSE_EVENT \
+// do {\
+// TRACKMOUSEEVENT __tme = { sizeof(__tme) };\
+// __tme.dwFlags = TME_LEAVE;\
+// __tme.hwndTrack = hwnd;\
+// TrackMouseEvent(&__tme); } while (0)
 LRESULT CALLBACK btn_proc(HWND hwnd, UINT umsg, WPARAM wparam, LPARAM lparam)
 {
+    static int is_mousehovered = 0;
+    static HWND mousehovered_window_handle = NULL;
     switch (umsg)
-    {
-        // case WM_CREATE:
+        {
+        // case WM_MOUSEMOVE:
         // {
-        //     // MessageBox(hwnd, L"on create", L"kkew", MB_OK);
-        //     // layout_element_t* elem = get_element(&mesh, hwnd);
-        //     // if (elem == NULL)
-        //     // {
-        //     //     MessageBox(NULL, L"can't get elem", L"kkew", MB_OK);
-        //     //     return 0;
-        //     // }
-        //     // // SetWindowPos(hwnd, NULL, elem->layout_position.x, elem->layout_position.y, elem->layout_size.x, elem->layout_size.y, SWP_SHOWWINDOW);
-        //     // SetWindowPos(hwnd, NULL, 100,100,50,50, SWP_SHOWWINDOW);
-        //     // RedrawWindow(hwnd, NULL, NULL, RDW_UPDATENOW);
-        //     return 0;
-        // }
+        //     TRACK_MOUSE_EVENT;
+        //     if (mousehovered_window_handle != hwnd);
+        //     {
+        //         mousehovered_window_handle = hwnd;
+        //     }
+        // } break;
+        // case WM_MOUSELEAVE:
+        // {
+        //     mousehovered_window_handle = NULL;
+        //     RedrawWindow(hwnd, NULL, NULL, RDW_INVALIDATE);
+        // } break;
         case WM_LBUTTONDOWN:
         {
-            // SetWindowPos(hwnd, NULL, 100,100,50,50, SWP_SHOWWINDOW | SWP_NOACTIVATE);
-            // UpdateWindow(hwnd);
-            // RedrawWindow(hwnd, NULL, NULL, RDW_UPDATENOW | RDW_INVALIDATE);
-            // layout_element_t* elem = get_element(&mesh, hwnd);
-            // if (elem == NULL)
+            // if(mousehovered_window_handle == hwnd)
             // {
-            //     MessageBox(NULL, L"can't get elem", L"kkew", MB_OK);
-            //     return 0;
+                layout_element_t* elem = get_element(&mesh, hwnd);
+                if (elem == NULL)
+                {
+                    MessageBox(NULL, L"can't get elem", L"kkew", MB_OK);
+                    return 0;
+                }
+            RECT rect;
+            GetWindowRect(hwnd, &rect);
+                wsprintf(buffer, L"pos:%d:%d\nsiz:%d:%d\nmes:%d,%d\nmes_s:%d:%d\nwnd_s:%d\nwnd_p:%d:%d\n",elem->layout_position.x, elem->layout_position.y,
+                                    elem->layout_size.x, elem->layout_size.y, elem->layout_position_in_mesh.x,elem->layout_position_in_mesh.y,
+                                    mesh.mesh_size.x, mesh.mesh_size.y, abs(rect.right - rect.left), rect.left, rect.top);
+                SetWindowPos(hwnd, NULL, elem->layout_position.x, elem->layout_position.y, elem->wnd_desc.window_size.x, elem->wnd_desc.window_size.y, SWP_SHOWWINDOW | SWP_NOACTIVATE);
+                MessageBox(hwnd, buffer, L"laout elem", MB_OK);
             // }
-            // SetWindowPos(hwnd, NULL, elem->layout_position.x, elem->layout_position.y, elem->layout_size.x, elem->layout_size.y, SWP_NOMOVE | SWP_NOSIZE);
-            return 0;
-        }
+        } break;
+        case WM_SIZE:
+        {
+            layout_element_t* elem = get_element(&mesh, hwnd);
+            if (elem == NULL)
+            {
+                MessageBox(NULL, L"can't get elem", L"kkew", MB_OK);
+                return 0;
+            }
+            SetWindowPos(hwnd, NULL, elem->layout_position.x, elem->layout_position.y, elem->wnd_desc.window_size.x, elem->wnd_desc.window_size.y, SWP_SHOWWINDOW | SWP_NOACTIVATE);
+            RedrawWindow(hwnd, NULL, NULL, RDW_UPDATENOW | RDW_INVALIDATE);
+        } break;
     }
     return CallWindowProc((WNDPROC)origin_btn_proc, hwnd, umsg, wparam, lparam);
 }
 
+BOOL CALLBACK EnumChildProc(HWND hwnd, LPARAM lparam)
+{
+    SendMessage(hwnd, WM_SIZE, 0, lparam);
+    return TRUE;
+}
+//mesh example
 // #include "layout.h"
 // #include <stdio.h>
 
