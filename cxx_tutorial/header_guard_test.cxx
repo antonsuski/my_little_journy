0adb2d1c10115fa163bc04bf4f8d04cf8ff6d4a9
Anton Syski <antonsyski@gmail.com>
2023-07-11 14:33:36 +0300

    cleaning

--- cxx_tutorial/header_guard_test.cxx
+++
@@ -1,9 +0,0 @@
-#include "header_guard_test.hxx"
-int get_some_thing()
-{
-  return 4;
-}
-int get(in// t value)
-// {
-//   return 5;
-// }
--- cxx_tutorial/header_guard_test.hxx
+++
@@ -1,8 +0,0 @@
-#ifndef HEADER_GUARD_H
-#define HRADER_GUADR_H
-
-int get_some_thing();
-
-int get(int value);
-
-#endif
